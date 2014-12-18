ff70270f5c205387923f7bd7b11453c851df4a29
Yann Guenver <sabrac72@gmail.com>
2014-12-18 11:03:49 +0100

    Delete file.h

--- file.h
+++
@@ -1,8 +0,0 @@
-
-typedef struct { int X ; int Y ;}t_coord;
-void initFile(); //initialisation la file à vide
-void ajouterFile(t_coord a);	//ajoute la valeur en queue de file
-void retirerFile(t_coord * val);	//retire la valeur en tête de file et l'affecte à val
-int fileVide();	//délivre vrai si la file est vide, faux sinon
-void afficher_queueFile();
-void afficher_elt(t_coord val);
