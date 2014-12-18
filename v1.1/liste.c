eb67ea0fd1fd502c131475fc95de089aba433c1f
Baptiste <Bien-CV@users.noreply.github.com>
2014-12-18 10:55:12 +0100

    Update liste.c

--- v1.1/liste.c
+++ v1.1/liste.c
@@ -9,72 +9,72 @@ t_coord T[N_LISTE];
 /* Déclaration des indices de queue et d'élément courant */
 int queue, ec;
 
-void init_liste(void)
+void liste_init(void)
 /* Initialise la liste à vide */
 {
 	queue = -1;
 	ec = -1;
 }
 
-int liste_vide(void)
+int liste_est_vide(void)
 /* Rend vrai si la liste est vide, faux sinon */
 {
 	return (queue== -1);
 }
 
-int hors_liste(void)
+int liste_est_hors_liste(void)
 /* Rend vrai si l'elt courant est hors de la liste, faux sinon */
 {
 	return(ec < 0 || ec > queue);
 }
 
-void en_tete(void)
+void liste_en_tete(void)
 /* Positionne en tete de la liste */
 {
-	if(!liste_vide())
+	if(!liste_est_vide())
 		ec = 0;
 }
 
-void en_queue(void)
+void liste_en_queue(void)
 /* Positionne en queue de la liste */
 {
-	if(!liste_vide())
+	if(!liste_est_vide())
 		ec = queue;
 }
 
-void precedent(void)
-/* Positionne sur l'elt precedent*/
+void liste_precedent(void)
+/* Positionne sur l'elt liste_precedent*/
 {
-	if(!hors_liste())
+	if(!liste_est_hors_liste())
 		ec--;
 }
 
-void suivant(void)
-/* Positionne sur l'elt suivant*/
+void liste_suivant(void)
+/* Positionne sur l'elt liste_suivant*/
 {
-	if(!hors_liste())
+	if(!liste_est_hors_liste())
 		ec++;
 }
 
-void valeur_elt(t_coord* e)
+void liste_valeur_elt(t_coord* e)
 /* Renvoie dans e la veleur de l'elt courant */
 {
-	if(!hors_liste())
+	if(!liste_est_hors_liste())
 		*e= T[ec];
 }
 
-void modif_elt(t_coord v)
+void liste_modif_elt(t_coord v)
 /* Affecte v à l'elt courant */
 {
-	if(!hors_liste())
+	if(!liste_est_hors_liste())
 		T[ec]=v;
 }
 
-void oter_elt(void)
-/* Supprime l'elt courant et positionne sur le precedent */
+void liste_oter_elt(void)
+/* Supprime l'elt courant et positionne sur le liste_precedent */
 {
 	int i;
-	if(!hors_liste())
+	if(!liste_est_hors_liste())
 	{
 		for(i=ec;i<queue;i++)
 			T[i]=T[i+1];
@@ -82,16 +82,16 @@ void oter_elt(void)
 	}
 }
 
-void ajout_droit(t_coord e)
+void liste_ajout_droit(t_coord e)
 /* Ajoute e a droite de l'elt courant */
 {
 	int i;
-    if(liste_vide())
+    if(liste_est_vide())
     {
 		T[0]=e;
 		ec++; queue++;
     }
-	if(!hors_liste())
+	if(!liste_est_hors_liste())
 	{
 		for(i=queue;i>ec;i--)
 			T[i+1]=T[i];
@@ -100,15 +100,15 @@ void ajout_droit(t_coord e)
 	}
 }
 
-void ajout_gauche(t_coord e)
+void liste_ajout_gauche(t_coord e)
 /* Ajoute e a gauche de l'elt courant */
 {
 	int i;
 
-	if(liste_vide())
+	if(liste_est_vide())
 		ec++;
 
-	if(liste_vide()||!hors_liste())
+	if(liste_est_vide()||!liste_est_hors_liste())
 	{
 		for(i=queue;i>=ec;i--)
 			T[i+1]=T[i];
@@ -117,30 +117,30 @@ void ajout_gauche(t_coord e)
 	}
 }
 
-void afficher_liste()
+void liste_afficher_contenu()
 {
     t_coord coord;
-    en_tete();
-    //suivant();
+    liste_en_tete();
+    //liste_suivant();
     printf("debut liste \n");
-    while(!hors_liste())
+    while(!liste_est_hors_liste())
     {
-        valeur_elt(&coord);
+        liste_valeur_elt(&coord);
         printf("%i * %i\n\n", coord.X, coord.Y);
-        suivant();
+        liste_suivant();
     }
     printf("fin liste \n");
 }
-void afficher_queue()
+void liste_afficher_queue()
 {
     t_coord val;
-    en_tete();
-    valeur_elt(&val);
+    liste_en_tete();
+    liste_valeur_elt(&val);
     printf("%i %i\n", val.X, val.Y);
 	//printf("\n%i %i\n",queue,ec);
 }
 
-void suppr_doublon()
+void liste_suppr_doublon()
 {
     int indice, indice1, indice2 = 0;
     t_coord valcour;
@@ -148,32 +148,32 @@ void suppr_doublon()
 
     for(indice1 = 0; indice1 <= queue; indice1++)
     {
-        en_tete();
+        liste_en_tete();
         for(indice = 0; indice < indice1; indice++)
-            suivant();
-        valeur_elt(&valcour);
-        suivant();
-        while(!hors_liste())
+            liste_suivant();
+        liste_valeur_elt(&valcour);
+        liste_suivant();
+        while(!liste_est_hors_liste())
         {
-            valeur_elt(&verif);
+            liste_valeur_elt(&verif);
             if(valcour.X == verif.X && valcour.Y == verif.Y)
-                oter_elt();
-            suivant();
+                liste_oter_elt();
+            liste_suivant();
             indice2++;
         }
     }
 }
 
-int calculerElemListe()
+int liste_calculer_nombre_elements()
 {
     int nb=0;
-    if(!liste_vide())
+    if(!liste_est_vide())
     {
-        en_tete();
-        while(!hors_liste())
+        liste_en_tete();
+        while(!liste_est_hors_liste())
         {
             nb++;
-            suivant();
+            liste_suivant();
         }
     }
     return nb;
